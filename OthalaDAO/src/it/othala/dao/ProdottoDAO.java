00a014bb3f35aa7fef7053addb76a6fc036414f8
simone.bagnolesi <simone.bagnolesi@gmail.com>
2014-02-07 16:25:01 +0000

    forza zio can

---
+++ OthalaDAO/src/it/othala/dao/ProdottoDAO.java
@@ -0,0 +1,9 @@
+package it.othala.dao;
+
+import it.othala.dao.interfaces.IProdottoDAO;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+
+public class ProdottoDAO extends SqlSessionDaoSupport implements IProdottoDAO {
+
+}
