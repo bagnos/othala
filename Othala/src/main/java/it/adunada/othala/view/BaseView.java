420aaa7ab0059ecc0263b18cf6babbf1a4bda661
simone.bagnolesi@gmail.com <simone.bagnolesi@gmail.com@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-02-11 16:26:14 +0000

    d

--- Othala/src/main/java/it/adunada/othala/view/BaseView.java
+++ Othala/src/main/java/it/adunada/othala/view/BaseView.java
@@ -2,9 +2,10 @@ package it.adunada.othala.view;
 
 public abstract class BaseView {
 	/**
-	 * inserire nella parte xhtml prima di h:head {@code <f:metadata>
-	 * <f:viewAction action="# sampleView.doInit}" /> </f:metadata>}
-	 * il metodo doInit verrà inocato alla prima richesta sulla pagina, no postback di default. 
+	 *dentro il content del .xhtml inserire {@code <f:metadata>
+	 * <f:viewAction action="sampleView.doInit" /> </f:metadata>}
+	 * il metodo doInit verrà inocato alla prima richesta sulla pagina, no postback di default.
+	 * Può essere utilizzato per inizializzare la view. 
 	 * @return
 	 */
 	public abstract String doInit();
