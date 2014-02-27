cc0ea9f17f272534639a6b64b7aaf771f2838e0b
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-02-27 12:21:43 +0000

    s

--- Othala/src/main/java/it/othala/account/view/AccessView.java
+++ Othala/src/main/java/it/othala/account/view/AccessView.java
@@ -4,20 +4,28 @@ package it.othala.account.view;
 
 
 import it.othala.account.model.AccessBean;
-import it.othala.account.model.RegistrationBean;
 import it.othala.view.BaseView;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.event.ActionEvent;
+import javax.faces.event.ActionListener;
 
 @ManagedBean
 public class AccessView extends BaseView{
 	
 	@ManagedProperty(value = "#{accessBean}")
 	private AccessBean accessBean;
+	private boolean renderClient;
 
 	
+	public boolean isRenderClient() {
+		return renderClient;
+	}
+
+	public void setRenderClient(boolean renderClient) {
+		this.renderClient = renderClient;
+	}
 
 	public AccessBean getAccessBean() {
 		return accessBean;
@@ -30,9 +38,32 @@ public class AccessView extends BaseView{
 	@Override
 	public String doInit() {
 		// TODO Auto-generated method stub
+		accessBean.setStaySignIn(true);
 		return null;
 	}
 	
+	public String login()
+	{
+		renderClient=true;
+		return null;
+	}
+	
+	public String registration()
+	{
+		return null;
+	}
+	
+	public String logout()
+	{
+		renderClient=false;
+		return null;
+	}
+	
+	public void logout(ActionEvent e)
+	{
+		renderClient=false;
+		
+	}
 	
 
 }
