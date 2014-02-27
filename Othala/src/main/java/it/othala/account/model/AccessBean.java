0ad38ba4a25c0fa4d0ff7f18bcb7b6403a7ee9b2
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-02-27 12:21:43 +0000

    s

--- Othala/src/main/java/it/othala/account/model/AccessBean.java
+++ Othala/src/main/java/it/othala/account/model/AccessBean.java
@@ -14,6 +14,26 @@ public class AccessBean extends BaseBean {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	
+	private String email;
+	private String psw;
+	private boolean staySignIn;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPsw() {
+		return psw;
+	}
+	public void setPsw(String psw) {
+		this.psw = psw;
+	}
+	public boolean isStaySignIn() {
+		return staySignIn;
+	}
+	public void setStaySignIn(boolean staySignIn) {
+		this.staySignIn = staySignIn;
+	}
 	
 }
