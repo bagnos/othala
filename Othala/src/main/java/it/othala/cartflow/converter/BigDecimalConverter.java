3b0ec23ca3be959affd3459d8405f1d0a26abc90
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-03-20 12:08:05 +0000

    s

---
+++ Othala/src/main/java/it/othala/cartflow/converter/BigDecimalConverter.java
@@ -0,0 +1,35 @@
+package it.othala.cartflow.converter;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter("it.othala.BigDecimalConverter")
+public class BigDecimalConverter implements Converter {
+
+	NumberFormat n = NumberFormat.getCurrencyInstance(Locale.ITALY);
+	
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component,
+			String value) {
+		// TODO Auto-generated method stub
+		return new BigDecimal(value);
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component,
+			Object value) {
+		// TODO Auto-generated method stub
+		BigDecimal scaleBD=((BigDecimal)value).setScale(2, RoundingMode.HALF_UP);
+		Double valueDbl=scaleBD.doubleValue();
+		String s = n.format(valueDbl); 
+		return s;
+	}
+
+}
