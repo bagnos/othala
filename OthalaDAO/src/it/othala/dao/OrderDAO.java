2e4fc39bbd6f4f11fe2ea63f96f355fe498031dc
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-03-21 16:50:13 +0000

    a

---
+++ OthalaDAO/src/it/othala/dao/OrderDAO.java
@@ -0,0 +1,57 @@
+package it.othala.dao;
+
+import it.othala.dao.interfaces.IOrderDAO;
+import it.othala.dto.OrderFullDTO;
+
+import java.sql.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+
+public class OrderDAO extends SqlSessionDaoSupport implements IOrderDAO  {
+
+	@Override
+	public List<OrderFullDTO> getOrders(Integer idOrder, Integer idUser,
+			Integer idStato, Date dtDatada, Date dtDataA) {
+		
+		HashMap<String, Object> mapOrder = new HashMap<>();
+		mapOrder.put("idOrder", idOrder);
+		mapOrder.put("idUser", idUser);
+		mapOrder.put("idStato", idStato);
+		mapOrder.put("dtDatada", dtDatada);
+		mapOrder.put("dtDataA", dtDataA);
+
+		// recupero prodotti
+		List<OrderFullDTO> listOrder = getSqlSession().selectList(
+				"it.othala.order.queries.listOrder", mapOrder);
+		
+		return listOrder;
+	}
+
+	@Override
+	public Integer insertOrder(OrderFullDTO orderFull) {
+		
+		getSqlSession().insert("it.othala.order.queries.insertOrder",
+				orderFull);
+		
+		return orderFull.getIdOrder();
+	}
+
+	@Override
+	public void insertOrdersArticles(OrderFullDTO orderFull) {
+		
+		getSqlSession().insert("it.othala.order.queries.insertOrdersArticles",
+				orderFull);
+		
+		}
+
+	@Override
+	public void insertStatesOrders(OrderFullDTO orderFull) {
+		
+		getSqlSession().insert("it.othala.order.queries.insertStatesOrders",
+				orderFull);
+		
+		}
+
+}
