7dd1153941e7b894c93c3c7cc1a58524158c1866
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-03-25 23:37:35 +0000

    s

--- Othala/src/main/java/it/othala/cartflow/view/CartChoice2View.java
+++ Othala/src/main/java/it/othala/cartflow/view/CartChoice2View.java
@@ -1,13 +1,17 @@
 package it.othala.cartflow.view;
 
 import it.othala.cartflow.model.CartFlowBean;
+import it.othala.dto.ArticleFullDTO;
 import it.othala.dto.ProductDTO;
 import it.othala.dto.ProductFullDTO;
 import it.othala.service.factory.OthalaFactory;
 import it.othala.view.BaseView;
+import it.othala.web.utils.OthalaUtil;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import javax.faces.model.SelectItem;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -17,6 +21,21 @@ public class CartChoice2View extends BaseView {
 
 	@Inject
 	private CartFlowBean flowBean;
+	private List<SelectItem> sizeItems;
+	private ArticleFullDTO articleSel;
+
+	
+	public ArticleFullDTO getArticleSel() {
+		return articleSel;
+	}
+
+	public void setArticleSel(ArticleFullDTO articleSel) {
+		this.articleSel = articleSel;
+	}
+
+	public List<SelectItem> getSizeItems() {
+		return sizeItems;
+	}
 
 	public CartFlowBean getFlowBean() {
 		return flowBean;
@@ -36,7 +55,6 @@ public class CartChoice2View extends BaseView {
 		this.idProduct = idProduct;
 	}
 
-
 	public String doInit() {
 		// TODO Auto-generated method stub
 
@@ -46,13 +64,20 @@ public class CartChoice2View extends BaseView {
 				break;
 			}
 		}
-		
-		ProductFullDTO prdFull=OthalaFactory.getProductServiceInstance().getProductFull(getLang(), idProduct);
-				
-				flowBean.setDetailProductFull(prdFull);
 
+		ProductFullDTO prdFull = OthalaFactory.getProductServiceInstance().getProductFull(getLang(), idProduct);
+
+		flowBean.setDetailProductFull(prdFull);
+
+		sizeItems = new ArrayList<>();
+		sizeItems.add(new SelectItem(-1, OthalaUtil.getWordBundle("catalog_chooseSize")));
+		for (ArticleFullDTO art : prdFull.getArticles()) {
+			sizeItems.add(new SelectItem(art, art.getTxSize()));
+		}
 
 		return null;
 	}
+	
+	
 
 }
