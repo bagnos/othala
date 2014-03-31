97f0be896bcc3b0f65cb6a2eb2c1cd856fb48e9c
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-03-31 08:40:13 +0000

    a

--- OthalaService/src/it/othala/service/interfaces/IOrderService.java
+++ OthalaService/src/it/othala/service/interfaces/IOrderService.java
@@ -1,6 +1,8 @@
 package it.othala.service.interfaces;
 
+import it.othala.account.execption.MailNotSendException;
 import it.othala.dto.OrderFullDTO;
+import it.othala.dto.StateOrderDTO;
 
 import java.sql.Date;
 import java.util.List;
@@ -9,6 +11,8 @@ public interface IOrderService {
 	
 	public List<OrderFullDTO> getOrders(Integer Order,Integer User,Integer StatoOrdine, Date DataIniPeriodo, Date DataFinPeriodo);
 	
-	public Integer insertOrder(OrderFullDTO orderFull);
+	public Integer insertOrder(OrderFullDTO orderFull) throws MailNotSendException;
+	
+	public void updateOrder(StateOrderDTO stateOrder);
 	
 }
