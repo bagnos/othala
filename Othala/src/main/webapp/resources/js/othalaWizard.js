ce84b0868d6cfc50583d61c0d7fec8b0574cb1c2
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-04-09 11:22:31 +0000

    d

--- Othala/src/main/webapp/resources/js/othalaWizard.js
+++ Othala/src/main/webapp/resources/js/othalaWizard.js
@@ -1,35 +1,45 @@
 $(document).ready(function() {
 
-	
-	  	$('#rootwizard').bootstrapWizard({'tabClass': 'bwizard-steps',onTabClick: function(tab, navigation, index) {
-		
-		return false;
-			
-	}});
+	$('#rootwizard').bootstrapWizard({
+		'tabClass' : 'bwizard-steps',
+		onTabClick : function(tab, navigation, index) {
 
-	if ($('#user').length > 0)
-		{
-
-  	$('.pager.wizard li.next a').click();
-
+			return false;
 
 		}
-	else
-		{
-	  	$('.pager.wizard li.next a').click();
+	});
 
-	  	$('.pager.wizard li.previous a').click();
-						
-		}
-  	
+	if ($('#user').length > 0) {
+
+		$('.pager.wizard li.next a').click();
+
+	} else {
+		$('.pager.wizard li.next a').click();
+
+		$('.pager.wizard li.previous a').click();
+
+	}
+	enabledPayment();
+
+
 });
 
 function changePage(xhr, status, args) {
 
-    if (args.validationFailed) {
-    	
-    } 
-    else
-        {
-    	$('.pager.wizard li.next a').click();}
+	if (args.validationFailed) {
+
+	} else {
+		$('.pager.wizard li.next a').click();
+	}
+}
+
+function enabledPayment() {
+	
+	$("input[name='optionsRadiosPay']").change(function() {
+		
+		$('#pay-cart').removeAttr('disabled');
+		$('#pay-cart').removeAttr('aria-disabled');
+		$("#pay-cart").removeClass("ui-state-disabled");
+	});
+
 }
