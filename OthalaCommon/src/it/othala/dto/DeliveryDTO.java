66519d739b5ff6ca3d85b7edc12b7fd53cb0d3f0
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-04-11 14:43:45 +0000

    a

---
+++ OthalaCommon/src/it/othala/dto/DeliveryDTO.java
@@ -0,0 +1,27 @@
+package it.othala.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class DeliveryDTO implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private List <DeliveryAddressDTO> indirizzo; 
+	public List<DeliveryAddressDTO> getIndirizzo() {
+		return indirizzo;
+	}
+	public void setIndirizzo(List<DeliveryAddressDTO> indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+	public List<DeliveryCostDTO> getSpeseSpedizione() {
+		return speseSpedizione;
+	}
+	public void setSpeseSpedizione(List<DeliveryCostDTO> speseSpedizione) {
+		this.speseSpedizione = speseSpedizione;
+	}
+	private List <DeliveryCostDTO> speseSpedizione;
+	
+}
