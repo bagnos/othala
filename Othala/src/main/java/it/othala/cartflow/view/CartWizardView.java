e1746ac3cfdd3e9616b782f03f7f17b4a4f9a7e5
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-05-02 09:41:11 +0000

    d

--- Othala/src/main/java/it/othala/cartflow/view/CartWizardView.java
+++ Othala/src/main/java/it/othala/cartflow/view/CartWizardView.java
@@ -1,26 +1,21 @@
 package it.othala.cartflow.view;
 
-import java.io.IOException;
-
 import it.othala.account.model.CustomerLoginBean;
 import it.othala.cartflow.model.CartFlowBean;
 import it.othala.dto.DeliveryAddressDTO;
 import it.othala.dto.DeliveryDTO;
 import it.othala.dto.OrderFullDTO;
-import it.othala.enums.TypeAddress;
 import it.othala.payment.paypal.PayPalWrapper;
 import it.othala.payment.paypal.SetExpressCheckoutDTO;
-import it.othala.service.factory.OthalaFactory;
 import it.othala.view.BaseView;
 import it.othala.web.utils.OthalaUtil;
 
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.faces.event.AjaxBehaviorEvent;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-
-import org.apache.catalina.filters.SetCharacterEncodingFilter;
 
 @Named
 @ViewScoped
@@ -31,6 +26,30 @@ public class CartWizardView extends BaseView {
 
 	@Inject
 	private CustomerLoginBean loginBean;
+
+	private DeliveryDTO deliveryDTO;
+	private int addressFat;
+	private int addressSpe;
+
+	public int getAddressFat() {
+		return addressFat;
+	}
+
+	public void setAddressFat(int addressFat) {
+		this.addressFat = addressFat;
+	}
+
+	public int getAddressSpe() {
+		return addressSpe;
+	}
+
+	public void setAddressSpe(int addressSpe) {
+		this.addressSpe = addressSpe;
+	}
+
+	public DeliveryDTO getDeliveryDTO() {
+		return deliveryDTO;
+	}
 
 	public String doInit() {
 		// TODO Auto-generated method stub
@@ -43,15 +62,48 @@ public class CartWizardView extends BaseView {
 	}
 
 	private void retrieveAddresses() {
-		if (cart.getAddressFat().getUserId() == null && cart.getAddressSpe().getUserId() == null) {
-			DeliveryDTO deliveryDTO = OthalaFactory.getOrderServiceInstance().getDeliveryInfo(loginBean.getEmail());
-			for (DeliveryAddressDTO addr : deliveryDTO.getIndirizzo()) {
-				if (addr.getTypeAddress().intValue() == TypeAddress.FATTURAZIONE.getAddress()) {
-					cart.setAddressFat(addr);
-				} else if (addr.getTypeAddress().intValue() == TypeAddress.SPEDIZIONE.getAddress()) {
-					cart.setAddressSpe(addr);
-				}
-			}
+
+		// deliveryDTO =
+		// OthalaFactory.getOrderServiceInstance().getDeliveryInfo(loginBean.getEmail());
+		deliveryDTO = new DeliveryDTO();
+		DeliveryAddressDTO addr = new DeliveryAddressDTO();
+		addr.setCap("54100");
+		addr.setCognome("1");
+		addr.setComune("2");
+		addr.setEtichetta("casa");
+		addr.setIdAddress(1);
+		addr.setNazione("IT");
+		addr.setNome("2");
+		addr.setProvincia("FI");
+		addr.setVia("via del cazzo");
+		deliveryDTO.getIndirizzo().add(addr);
+
+		addr = new DeliveryAddressDTO();
+		addr.setCap("54100");
+		addr.setCognome("1");
+		addr.setComune("2");
+		addr.setEtichetta("lavoro");
+		addr.setIdAddress(2);
+		addr.setNazione("IT");
+		addr.setNome("2");
+		addr.setProvincia("FI");
+		addr.setVia("via del cazzo");
+		deliveryDTO.getIndirizzo().add(addr);
+		/*
+		 * for (DeliveryAddressDTO addr : deliveryDTO.getIndirizzo()) { if
+		 * (addr.getTypeAddress().intValue() ==
+		 * TypeAddress.FATTURAZIONE.getAddress()) { cart.setAddressFat(addr); }
+		 * else if (addr.getTypeAddress().intValue() ==
+		 * TypeAddress.SPEDIZIONE.getAddress()) { cart.setAddressSpe(addr); } }
+		 */
+
+	}
+
+	public void changeAddrFat(AjaxBehaviorEvent ev) {
+		if (addressFat == 0) {
+
+		} else {
+
 		}
 	}
 
@@ -63,22 +115,22 @@ public class CartWizardView extends BaseView {
 		PayPalWrapper pBd;
 		try {
 			pBd = new PayPalWrapper();
-			
-			//creazione ordine
-			OrderFullDTO order=new OrderFullDTO();			
+
+			// creazione ordine
+			OrderFullDTO order = new OrderFullDTO();
 			order.setIdOrder(122345);
-			
-			
-			//checkout paypall
-			SetExpressCheckoutDTO checkDTO = pBd.setExpressCheckout(cart,getLang(),order.getIdOrder().toString());
-			if (checkDTO!=null && checkDTO.getToken() != null) {
+
+			// checkout paypall
+			SetExpressCheckoutDTO checkDTO = pBd.setExpressCheckout(cart, getLang(), order.getIdOrder().toString());
+			if (checkDTO != null && checkDTO.getToken() != null) {
 				// return null;
 				FacesContext.getCurrentInstance().getExternalContext().redirect(checkDTO.getRedirectUrl());
-				
+
 			} else {
 				addError(null, OthalaUtil.getWordBundle("exception_payPalException"));
-				log.error(String.format("Paypal, SetExpressCheckout in errore:%s",checkDTO!=null?checkDTO.getKoMessage():"errore generico nella cominicazione con paypal"));
-				// return null; 
+				log.error(String.format("Paypal, SetExpressCheckout in errore:%s",
+						checkDTO != null ? checkDTO.getKoMessage() : "errore generico nella cominicazione con paypal"));
+				// return null;
 			}
 		} catch (Exception ex) {
 			// TODO Auto-generated catch block
--- Othala/src/main/java/it/othala/test/MailTest.java
+++ Othala/src/main/java/it/othala/test/MailTest.java
@@ -1,10 +1,20 @@
 package it.othala.test;
 
+import it.othala.account.execption.MailNotSendException;
+import it.othala.service.factory.OthalaFactory;
+
 import org.junit.Test;
 
 public class MailTest {
 	@Test
 	public void test() {
-	
+		
+		try {
+			OthalaFactory.getOrderServiceInstance().inviaMailDiConferma(null);
+		} catch (MailNotSendException e) {
+			// TODO Auto-generated catch block
+			
+			e.printStackTrace();
+		}
 	}
 }
