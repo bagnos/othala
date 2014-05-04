7a08b6f5933ee4bd3fc58d37306ef9eb07988e53
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-05-04 21:35:48 +0000

    s

--- Othala/src/main/java/it/othala/test/MailTest.java
+++ Othala/src/main/java/it/othala/test/MailTest.java
@@ -1,7 +1,17 @@
 package it.othala.test;
 
 import it.othala.account.execption.MailNotSendException;
+import it.othala.dto.ArticleFullDTO;
+import it.othala.dto.MailConfermaDTO;
+import it.othala.dto.OrderFullDTO;
+import it.othala.dto.ProductFullDTO;
 import it.othala.service.factory.OthalaFactory;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
 import org.junit.Test;
 
@@ -10,7 +20,29 @@ public class MailTest {
 	public void test() {
 		
 		try {
-			OthalaFactory.getOrderServiceInstance().inviaMailDiConferma(null);
+			List<OrderFullDTO> orders=OthalaFactory.getOrderServiceInstance().getOrders(1, "1", 2);
+			OrderFullDTO order=orders.get(0);
+	
+			
+			ProductFullDTO prd=new ProductFullDTO();
+			prd.setDescription("Prodotto del cazzo");
+			prd.setTxBrand("Brand del cazzo");
+			prd.setIdProduct(2);
+			prd.setPrice(new BigDecimal("30"));
+			order.setIdUser("simone.bagnolesi@gmail.com");
+			for (ArticleFullDTO art:order.getCart())
+			{
+				art.setPrdFullDTO(prd);
+				art.setQtBooked(1);
+			}
+			
+
+			
+			MailConfermaDTO mail=new MailConfermaDTO();
+			mail.setBasePathArticle("C:/progs/Tomcat/apache-tomcat-7.0.53/webapps/Othala/resources/images/cartThumbinals/");
+			mail.setPathImgLogo("C:/progs/Tomcat/apache-tomcat-7.0.53/webapps/Othala/resources/images/logo2-small.png");
+			mail.setPathImgPayment("C:/progs/Tomcat/apache-tomcat-7.0.53/webapps/Othala/resources/images/payment/paypal.gif");
+			OthalaFactory.getOrderServiceInstance().inviaMailDiConferma(order,mail);
 		} catch (MailNotSendException e) {
 			// TODO Auto-generated catch block
 			
---
+++ OthalaCommon/src/it/othala/dto/MailConfermaDTO.java
@@ -0,0 +1,27 @@
+package it.othala.dto;
+
+public class MailConfermaDTO {
+	private String pathImgLogo;
+	private String pathImgPayment;
+	private String basePathArticle;
+	
+	public String getPathImgLogo() {
+		return pathImgLogo;
+	}
+	public void setPathImgLogo(String pathImgLogo) {
+		this.pathImgLogo = pathImgLogo;
+	}
+	public String getPathImgPayment() {
+		return pathImgPayment;
+	}
+	public void setPathImgPayment(String pathImgPayment) {
+		this.pathImgPayment = pathImgPayment;
+	}
+	public String getBasePathArticle() {
+		return basePathArticle;
+	}
+	public void setBasePathArticle(String basePathArticle) {
+		this.basePathArticle = basePathArticle;
+	}
+	
+}
