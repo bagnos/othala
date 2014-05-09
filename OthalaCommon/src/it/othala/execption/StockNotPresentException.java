39ec910afb72c460edaeb0d6d38001ded2a84d8d
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-05-09 14:28:13 +0000

    s

---
+++ OthalaCommon/src/it/othala/execption/StockNotPresentException.java
@@ -0,0 +1,17 @@
+package it.othala.execption;
+
+public class StockNotPresentException extends OthalaException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static final String KEY_MESSAGE = "exception_stockNotPresentException";
+
+	public StockNotPresentException() {
+		super(KEY_MESSAGE, new Object[] {});
+		// TODO Auto-generated constructor stub
+
+	}
+
+}
