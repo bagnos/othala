9c8550b1c120f5283b1cc3a952318191f6cb9f96
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-05-12 07:54:19 +0000

    a

--- Othala/src/main/java/it/othala/test/OrderTest.java
+++ Othala/src/main/java/it/othala/test/OrderTest.java
@@ -1,16 +1,26 @@
 package it.othala.test;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
+import it.othala.account.execption.MailNotSendException;
+import it.othala.cartflow.converter.BigDecimalConverter;
 import it.othala.dto.ArticleFullDTO;
+import it.othala.dto.DeliveryAddressDTO;
+import it.othala.dto.DeliveryCostDTO;
 import it.othala.dto.OrderFullDTO;
+import it.othala.dto.ProductFullDTO;
+import it.othala.enums.TypeStateOrder;
+import it.othala.execption.OthalaException;
+import it.othala.execption.StockNotPresentException;
 import it.othala.service.factory.OthalaFactory;
 
 import org.junit.Test;
 
 public class OrderTest {
-	@Test
-	public void test() {
+	//@Test
+	public void test1() {
 		
 		try {
 			
@@ -25,5 +35,60 @@ public class OrderTest {
 			
 			e.printStackTrace();
 		}
+	}
+	//@Test
+	public void Test2(){
+		OrderFullDTO orderFull = new OrderFullDTO();
+		DeliveryAddressDTO deliveryAddress =  new DeliveryAddressDTO();
+		DeliveryAddressDTO billingAddress = new DeliveryAddressDTO();
+		DeliveryCostDTO cost =  new DeliveryCostDTO();
+		ArticleFullDTO art =  new ArticleFullDTO();
+		ProductFullDTO prod =  new ProductFullDTO();
+		List<ArticleFullDTO> cart =  new ArrayList<ArticleFullDTO>();
+		deliveryAddress.setIdAddress(1);
+		billingAddress.setIdAddress(1);
+		cost.setIdDeliveryCost(1);
+		art.setPgArticle(1);
+		art.setQtBooked(1);
+		prod.setIdProduct(1);
+		art.setPrdFullDTO(prod);
+		cart.add(art);
+		
+		orderFull.setBillingAddress(billingAddress);
+		orderFull.setShippingAddress(deliveryAddress);
+		orderFull.setCart(cart);
+		orderFull.setSpeseSpedizione(cost);
+		orderFull.setIdUser("1");
+		
+		BigDecimal bd = new BigDecimal(100);
+		orderFull.setImOrdine(bd);
+		
+		try{
+			OthalaFactory.getOrderServiceInstance().insertOrder(orderFull);
+		}
+		catch(OthalaException oex){
+			
+			oex.printStackTrace();
+		}
+
+		
+	}
+	//@Test
+	public void Test3(){
+		try{
+		OthalaFactory.getOrderServiceInstance().confirmOrderPayment("ABC1234", 2, TypeStateOrder.PAGATO);
+		}
+		catch(StockNotPresentException ex){
+			ex.printStackTrace();
+		}
+
+		
+	}
+	@Test
+	public void Test4(){
+		
+		OthalaFactory.getOrderServiceInstance().confirmOrderDelivery("PPPPP1234",2);
+		
+		
 	}
 }
