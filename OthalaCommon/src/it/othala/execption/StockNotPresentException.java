409ec17ec2df4c7665bfae0f59e41f904158417c
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-05-12 07:59:01 +0000

    d

--- OthalaCommon/src/it/othala/execption/StockNotPresentException.java
+++ OthalaCommon/src/it/othala/execption/StockNotPresentException.java
@@ -1,5 +1,9 @@
 package it.othala.execption;
 
+import java.util.List;
+
+import org.springframework.util.StringUtils;
+
 public class StockNotPresentException extends OthalaException {
 
 	/**
@@ -8,8 +12,9 @@ public class StockNotPresentException extends OthalaException {
 	private static final long serialVersionUID = 1L;
 	private static final String KEY_MESSAGE = "exception_stockNotPresentException";
 
-	public StockNotPresentException() {
-		super(KEY_MESSAGE, new Object[] {});
+	public StockNotPresentException(List<String> productsNoStocked) {
+		
+		super(KEY_MESSAGE, new Object[] {StringUtils.collectionToCommaDelimitedString(productsNoStocked)});
 		// TODO Auto-generated constructor stub
 
 	}
