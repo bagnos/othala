b205831055779e4132b04644dcc9e113158d95f5
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-05-13 15:01:22 +0000

    enum stati con payapl

--- OthalaCommon/src/it/othala/enums/TypeStateOrder.java
+++ OthalaCommon/src/it/othala/enums/TypeStateOrder.java
@@ -1,7 +1,8 @@
 package it.othala.enums;
 
 public enum TypeStateOrder {
-	INSERITO(1), IN_LAVORAZIONE(2), PAGATO(3), SPEDITO(4), ANNULLATO(5), RECESSO(6);
+	INSERITO(1), CANCELEDREVERSAL(2), COMPLETED(3), DENIED(4), EXPIRED(5), FAILED(6), INPROGRESS(7), PARTIALLYREFUNDED(
+			8), PENDING(9), REFUNDED(10), REVERSED(11), PROCESSED(12), VOIDED(13), COMPLETEDFUNDSHELD(14), SPEDITO(15);
 
 	private int stato;
 
@@ -12,17 +13,24 @@ public enum TypeStateOrder {
 	public int getState() {
 		return stato;
 	}
-	
-	public static TypeStateOrder valueOf(int idState)
-	{
-		for (TypeStateOrder type: TypeStateOrder.values())
-		{
-			if (type.getState()==idState)
-			{
+
+	public String getDescrState(int idState) {
+		String state = valueOf(idState).toString().replace("-", "");
+		return state;
+	}
+
+	public static TypeStateOrder valueOf(int idState) {
+		for (TypeStateOrder type : TypeStateOrder.values()) {
+			if (type.getState() == idState) {
 				return type;
 			}
 		}
 		return null;
 	}
-	
+
+	public static int getIdFromDescription(String state) {
+		TypeStateOrder type = TypeStateOrder.valueOf(state.replace("-", "").toUpperCase());
+		return type.getState();
+	}
+
 }
