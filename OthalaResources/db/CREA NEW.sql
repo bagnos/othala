068df0e659274b6b0683860df3769940a187b64f
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-05-20 13:15:12 +0000

    a

--- OthalaResources/db/CREA NEW.sql
+++ OthalaResources/db/CREA NEW.sql
@@ -489,7 +489,7 @@ DROP TABLE IF EXISTS `othala`.`States_Orders` ;
 CREATE  TABLE IF NOT EXISTS `othala`.`States_Orders` (
   `idOrder` INT NOT NULL ,
   `idStato` INT NOT NULL ,
-  `dtStato` DATE NULL ,
+  `dtStato` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
   `txNote` VARCHAR(100) NULL ,
   PRIMARY KEY (`idOrder`, `idStato`) ,
   INDEX `fk_StatesOrders_States1_idx` (`idStato` ASC) ,
