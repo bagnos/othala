d046157d8110b0dfbe686e805e1cdae26a208ec8
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-06-03 15:28:33 +0000

    s

---
+++ Othala/src/main/java/it/othala/merchant/model/MerchantBean.java
@@ -0,0 +1,12 @@
+package it.othala.merchant.model;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class MerchantBean implements Serializable {
+
+}
---
+++ Othala/src/main/java/it/othala/merchant/view/InsertProdottiView.java
@@ -0,0 +1,17 @@
+package it.othala.merchant.view;
+
+import it.othala.view.BaseView;
+
+import javax.inject.Named;
+
+@Named
+@javax.faces.view.ViewScoped
+public class InsertProdottiView extends BaseView {
+
+	@Override
+	public String doInit() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
