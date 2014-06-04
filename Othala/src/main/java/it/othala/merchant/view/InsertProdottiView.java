e2c54cc636853c49ed0d7839a815956eda91f81a
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-06-04 08:35:16 +0000

    S

--- Othala/src/main/java/it/othala/merchant/view/InsertProdottiView.java
+++ Othala/src/main/java/it/othala/merchant/view/InsertProdottiView.java
@@ -2,16 +2,42 @@ package it.othala.merchant.view;
 
 import it.othala.view.BaseView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Named;
 
 @Named
 @javax.faces.view.ViewScoped
 public class InsertProdottiView extends BaseView {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String gender;
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
 
 	@Override
 	public String doInit() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	public List<String> completeText(String query) {
+        List<String> results = new ArrayList<String>();
+        for(int i = 0; i < 10; i++) {
+            results.add(query + i);
+        }
+         
+        return results;
+    }
 
 }
