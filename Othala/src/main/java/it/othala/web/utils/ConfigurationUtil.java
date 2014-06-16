ce4c350b880dc905ad5410ee58321785ac9c109e
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-06-16 21:31:39 +0000

    d

--- Othala/src/main/java/it/othala/web/utils/ConfigurationUtil.java
+++ Othala/src/main/java/it/othala/web/utils/ConfigurationUtil.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.logging.Log;
