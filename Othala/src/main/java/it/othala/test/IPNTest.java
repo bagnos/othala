c0664871a2eb15589b86936e1876acae851f3ff0
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-06-18 22:49:06 +0000

    s

---
+++ Othala/src/main/java/it/othala/test/IPNTest.java
@@ -0,0 +1,27 @@
+package it.othala.test;
+
+import it.othala.account.execption.MailNotSendException;
+import it.othala.dto.ArticleFullDTO;
+import it.othala.dto.MailPropertiesDTO;
+import it.othala.dto.OrderFullDTO;
+import it.othala.dto.ProductFullDTO;
+import it.othala.dto.ProfilePayPalDTO;
+import it.othala.service.factory.OthalaFactory;
+import it.othala.web.utils.PayPalUtil;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+import org.junit.Test;
+
+public class IPNTest {
+	@Test
+	public void test() {
+
+		String orginalRequest = "residence_country=US&invoice=abc1234&address_city=San+Jose&first_name=John&payer_id=TESTBUYERID01&shipping=3.04&mc_fee=0.44&txn_id=683347834&receiver_email=seller%40paypalsandbox.com&quantity=1&custom=xyz123&payment_date=07%3A29%3A58+18+Jun+2014+PDT&address_country_code=US&address_zip=95131&tax=2.02&item_name=something&address_name=John+Smith&last_name=Smith&receiver_id=seller%40paypalsandbox.com&item_number=AK-1234&verify_sign=AbYqbFe-RIL51x.pDPGokmuBWtzqAtLO3UQ5Q4R2bOWDXmZFB1deAty0&address_country=United+States&payment_status=Completed&address_status=confirmed&business=seller%40paypalsandbox.com&payer_email=buyer%40paypalsandbox.com¬ify_version=2.1&txn_type=web_accept&test_ipn=1&payer_status=verified&mc_currency=USD&mc_gross=12.34&address_state=CA&mc_gross1=12.34&payment_type=echeck&address_street=123%2C+any+street";
+
+	}
+}
