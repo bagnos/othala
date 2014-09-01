663493a3492d60579a9e04de52e0b6600c7f5ffa
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-09-01 11:32:42 +0000

    a

--- OthalaService/src/it/othala/service/interfaces/IPaymentService.java
+++ OthalaService/src/it/othala/service/interfaces/IPaymentService.java
@@ -4,6 +4,7 @@ import it.othala.account.execption.MailNotSendException;
 import it.othala.dto.MailPropertiesDTO;
 import it.othala.dto.OrderFullDTO;
 import it.othala.dto.ProfilePayPalDTO;
+import it.othala.enums.TypeStateOrder;
 import it.othala.execption.OthalaException;
 import it.othala.execption.StockNotPresentException;
 import it.othala.payment.paypal.dto.DoExpressCheckoutPaymentDTO;
@@ -37,7 +38,7 @@ public interface IPaymentService {
 	public void sendMailAcceptedPyament(OrderFullDTO order, MailPropertiesDTO mailDTO, String status)
 			throws MailNotSendException;
 
-	public void sendMailAcceptedPyamentAfterPending(OrderFullDTO order, MailPropertiesDTO mailDTO)
+	public void sendMailAcceptedPyamentAfterPending(OrderFullDTO order, MailPropertiesDTO mailDTO, TypeStateOrder state)
 			throws MailNotSendException;
 
 	public SetExpressCheckoutDTO setExpressCheckout(OrderFullDTO order, ProfilePayPalDTO profile)
