490d9f26a9d233a04a029f2167706d85fa7f1809
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-09-18 13:34:27 +0000

    a

---
+++ OthalaCommon/src/it/othala/dto/RefoundFullDTO.java
@@ -0,0 +1,91 @@
+package it.othala.dto;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.omg.CORBA.portable.IndirectionException;
+
+public class RefoundFullDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Integer idRefound;
+	private Integer idOrder;
+	private String idUser;
+	private String idTransaction;
+	private BigDecimal imRefound;
+	private Integer idStato;
+	private String txStato;
+	private Timestamp dtStato;
+	private String txNote;
+	private List<ArticleFullDTO> cart;
+	
+	public Integer getIdRefound() {
+		return idRefound;
+	}
+	public void setIdRefound(Integer idRefound) {
+		this.idRefound = idRefound;
+	}
+	public Integer getIdOrder() {
+		return idOrder;
+	}
+	public void setIdOrder(Integer idOrder) {
+		this.idOrder = idOrder;
+	}
+	public String getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(String idUser) {
+		this.idUser = idUser;
+	}
+	public String getIdTransaction() {
+		return idTransaction;
+	}
+	public void setIdTransaction(String idTransaction) {
+		this.idTransaction = idTransaction;
+	}
+	public BigDecimal getImRefound() {
+		return imRefound;
+	}
+	public void setImRefound(BigDecimal imRefound) {
+		this.imRefound = imRefound;
+	}
+	public Integer getIdStato() {
+		return idStato;
+	}
+	public void setIdStato(Integer idStato) {
+		this.idStato = idStato;
+	}
+	public String getTxStato() {
+		return txStato;
+	}
+	public void setTxStato(String txStato) {
+		this.txStato = txStato;
+	}
+	public Timestamp getDtStato() {
+		return dtStato;
+	}
+	public void setDtStato(Timestamp dtStato) {
+		this.dtStato = dtStato;
+	}
+	public String getTxNote() {
+		return txNote;
+	}
+	public void setTxNote(String txNote) {
+		this.txNote = txNote;
+	}
+	public List<ArticleFullDTO> getCart() {
+		return cart;
+	}
+	public void setCart(List<ArticleFullDTO> cart) {
+		this.cart = cart;
+	}
+	
+	
+}
