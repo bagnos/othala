0d49411e73bb8ad1e408fb1763508597400b6a9e
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-09-24 21:44:50 +0000

    s

--- Othala/src/main/java/it/othala/merchant/view/HomeMerchantView.java
+++ Othala/src/main/java/it/othala/merchant/view/HomeMerchantView.java
@@ -28,28 +28,27 @@ public class HomeMerchantView extends BaseView {
 	public String doInit() {
 		// TODO Auto-generated method stub
 
-		
-		
-		if (getQueryStringParm("torna") == null || getQueryStringParm("spedito")!=null) {
+		if (getQueryStringParm("torna") == null || getQueryStringParm("spedito") != null
+				|| getQueryStringParm("rimborsato") != null) {
 			merchantBean.setOrdersCompleted(OthalaFactory.getOrderServiceInstance().getOrders(null, null,
 					TypeStateOrder.COMPLETED));
 
 			merchantBean.setOrdersPending(OthalaFactory.getOrderServiceInstance().getOrders(null, null,
 					TypeStateOrder.PENDING));
-			
-			
 
-			merchantBean.setOrderSelected(null);
-			
+			merchantBean.setRefoundRequest(OthalaFactory.getOrderServiceInstance().getRefounds(null, null, null,
+					TypeStateOrder.REQUEST_REFOUND, null));
+
+			merchantBean.setOrderSelected(null); 
+
 			merchantBean.setRefoundSelected(null);
 		}
 		return null;
 	}
-	
-	public void selectRightClick(org.primefaces.event.SelectEvent e)
-	{
-		Integer idOrder=(Integer) e.getComponent().getAttributes().get("idOrder");
-		
+
+	public void selectRightClick(org.primefaces.event.SelectEvent e) {
+		Integer idOrder = (Integer) e.getComponent().getAttributes().get("idOrder");
+
 	}
 
 }
