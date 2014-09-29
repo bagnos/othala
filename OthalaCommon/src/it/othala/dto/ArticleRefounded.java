ee8239fbdb1613742d0c9d3fc612f064804db177
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-09-29 14:23:43 +0000

    a

---
+++ OthalaCommon/src/it/othala/dto/ArticleRefounded.java
@@ -0,0 +1,48 @@
+package it.othala.dto;
+
+import java.io.Serializable;
+
+public class ArticleRefounded extends ArticleFullDTO {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String fgChangeRefound;
+	private String txChangeRefound;	
+	
+	public ArticleRefounded(){
+		super();
+	};
+	
+	public ArticleRefounded(ArticleFullDTO artFull){
+		this.setIdColor(artFull.getIdColor());
+		this.setIdSize(artFull.getIdSize());
+		this.setNote(artFull.getNote());
+		this.setPgArticle(artFull.getPgArticle());
+		this.setPrdFullDTO(artFull.getPrdFullDTO());
+		this.setQtBooked(artFull.getQtBooked());
+		this.setQtStock(artFull.getQtStock());
+		this.setShop(artFull.getShop());
+		this.setThumbnailsUrl(artFull.getThumbnailsUrl());
+		this.setTxBarCode(artFull.getTxBarCode());
+		this.setTxColor(artFull.getTxColor());
+		this.setTxSize(artFull.getTxSize());
+		
+	};
+	
+	public String getFgChangeRefound() {
+		return fgChangeRefound;
+	}
+	public void setFgChangeRefound(String fgChangeRefound) {
+		this.fgChangeRefound = fgChangeRefound;
+	}
+	public String getTxChangeRefound() {
+		return txChangeRefound;
+	}
+	public void setTxChangeRefound(String txChangeRefound) {
+		this.txChangeRefound = txChangeRefound;
+	}
+
+	
+}
