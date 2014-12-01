0b415ca554e254f70fc7aed4981bbb7d4b750fed
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2014-12-01 13:59:56 +0000

    w

--- OthalaMBeans/src/it/othala/model/LocaleManager.java
+++ OthalaMBeans/src/it/othala/model/LocaleManager.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.Locale;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
@@ -14,8 +15,15 @@ import javax.faces.context.FacesContext;
 public class LocaleManager {
 
     private Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+    @ManagedProperty(value="#{applicationBean}")
+	//@Inject
+	private ApplicationBean beanApplication;
 
-    public Locale getLocale() {
+    public void setBeanApplication(ApplicationBean beanApplication) {
+		this.beanApplication = beanApplication;
+	}
+
+	public Locale getLocale() {
         return locale;
     }
 
@@ -25,9 +33,11 @@ public class LocaleManager {
 
     public void changeLocale(String language) {
         locale = new Locale(language);
-        OthalaUtil.setResources(getLocale());
-        FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
-		Locale.setDefault(locale);
+        //OthalaUtil.setResources(getLocale()); 
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+        OthalaUtil.setLangFromContextJSF(locale.getLanguage());
+        beanApplication.resetDomain();
+        beanApplication.resetMenu();
     }
 
 }
