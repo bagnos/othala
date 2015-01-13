f4ac12beb44531bf4d6cefb93f43bd1ab71eae5b
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2015-01-13 14:19:11 +0000

    w

--- OthalaCommon/src/it/othala/dto/ArticleCashedDTO.java
+++ OthalaCommon/src/it/othala/dto/ArticleCashedDTO.java
@@ -3,7 +3,7 @@ package it.othala.dto;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
-public class ArticleCashed implements Serializable {
+public class ArticleCashedDTO implements Serializable {
 	/**
 	 * 
 	 */
