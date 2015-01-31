53f4f3d2763a5a4928dbe72e0f984b12a0cc0111
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2015-01-31 13:02:33 +0000

    A

---
+++ OthalaCommon/src/it/othala/external/dto/FidelityCardDegortesDTO.java
@@ -0,0 +1,14 @@
+package it.othala.external.dto;
+
+public class FidelityCardDegortesDTO {
+	
+	/*NumeroFidelity
+	TipoCarta
+	Cliente
+	DataEmissione
+	DataScadenza
+	ValoreSconto
+	StatoFidelity
+	BarCodeFidelity*/
+
+}
