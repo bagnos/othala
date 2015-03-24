3c31e4a5e5f28d6b6435093e507b969980b9b473
simone.bagnolesi <simone.bagnolesi@a3c0b6ad-1eda-1413-4776-d5a652e763a6>
2015-03-24 22:45:05 +0000

    s

--- OthalaService/src/it/othala/service/factory/OthalaFactory.java
+++ OthalaService/src/it/othala/service/factory/OthalaFactory.java
@@ -1,5 +1,6 @@
 package it.othala.service.factory;
 
+import it.othala.external.service.interfaces.IOthalaExternalServices;
 import it.othala.service.interfaces.IAccountService;
 import it.othala.service.interfaces.IMailService;
 import it.othala.service.interfaces.IMessageIpnService;
@@ -24,6 +25,7 @@ public class OthalaFactory {
 	private static ISiteImagesService siteImagesService;
 	private static IPaymentService paymentService;
 	private static IMessageIpnService messageIpnService;
+	private static IOthalaExternalServices externalService;
 
 	private static ApplicationContext getApplicationContext() {
 
@@ -32,8 +34,7 @@ public class OthalaFactory {
 				if (appContext == null)
 
 				{
-					appContext = new ClassPathXmlApplicationContext(
-							appContextCompletePath);
+					appContext = new ClassPathXmlApplicationContext(appContextCompletePath);
 				}
 
 			}
@@ -41,58 +42,58 @@ public class OthalaFactory {
 		return appContext;
 	}
 
+	public static IOthalaExternalServices getExternalServiceInstance() {
+		if (externalService == null) {
+			externalService = (IOthalaExternalServices) getApplicationContext().getBean("othalaExternalService");
+		}
+		return externalService;
+	}
+
 	public static IAccountService getAccountServiceInstance() {
 		if (accountService == null) {
-			accountService = (IAccountService) getApplicationContext().getBean(
-					"accountService");
+			accountService = (IAccountService) getApplicationContext().getBean("accountService");
 		}
 		return accountService;
 	}
 
 	public static IProductService getProductServiceInstance() {
 		if (productService == null) {
-			productService = (IProductService) getApplicationContext().getBean(
-					"productService");
+			productService = (IProductService) getApplicationContext().getBean("productService");
 		}
 		return productService;
 	}
 
 	public static IPaymentService getPaymentServiceInstance() {
 		if (paymentService == null) {
-			paymentService = (IPaymentService) getApplicationContext().getBean(
-					"paymentService");
+			paymentService = (IPaymentService) getApplicationContext().getBean("paymentService");
 		}
 		return paymentService;
 	}
 
 	public static IMailService getMailServiceInstance() {
 		if (mailService == null) {
-			mailService = (IMailService) getApplicationContext().getBean(
-					"mailService");
+			mailService = (IMailService) getApplicationContext().getBean("mailService");
 		}
 		return mailService;
 	}
 
 	public static IOrderService getOrderServiceInstance() {
 		if (orderService == null) {
-			orderService = (IOrderService) getApplicationContext().getBean(
-					"orderService");
+			orderService = (IOrderService) getApplicationContext().getBean("orderService");
 		}
 		return orderService;
 	}
 
 	public static ISiteImagesService getSiteImagesServiceInstance() {
 		if (siteImagesService == null) {
-			siteImagesService = (ISiteImagesService) getApplicationContext()
-					.getBean("siteImagesService");
+			siteImagesService = (ISiteImagesService) getApplicationContext().getBean("siteImagesService");
 		}
 		return siteImagesService;
 	}
-	
+
 	public static IMessageIpnService getMessageIpnServiceInstance() {
 		if (messageIpnService == null) {
-			messageIpnService = (IMessageIpnService) getApplicationContext()
-					.getBean("messageIpnService");
+			messageIpnService = (IMessageIpnService) getApplicationContext().getBean("messageIpnService");
 		}
 		return messageIpnService;
 	}
