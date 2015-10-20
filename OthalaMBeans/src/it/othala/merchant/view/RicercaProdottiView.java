72c3374e94c437526ec6198184fc1ab302999ad3
jacopo <jacopo.frediani@gmail.com>
2015-10-20 13:34:54 +0200

    ok

--- OthalaMBeans/src/it/othala/merchant/view/RicercaProdottiView.java
+++ OthalaMBeans/src/it/othala/merchant/view/RicercaProdottiView.java
@@ -12,6 +12,7 @@ import it.othala.service.factory.OthalaFactory;
 import it.othala.view.BaseView;
 import it.othala.web.utils.ResizeImageUtil;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +20,7 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.faces.event.AjaxBehaviorEvent;
 import javax.faces.model.SelectItem;
@@ -42,7 +44,6 @@ public class RicercaProdottiView extends BaseView {
 	private Integer idCampaign;
 	List<CampaignDTO> campaigns;
 	private CampaignDTO campaignDTO = new CampaignDTO();
-	
 
 	public Integer getIdCampaign() {
 		return idCampaign;
@@ -59,8 +60,6 @@ public class RicercaProdottiView extends BaseView {
 	public void setFile(UploadedFile file) {
 		this.file = file;
 	}
-
-	
 
 	public CampaignDTO getCampaignDTO() {
 		return campaignDTO;
@@ -107,8 +106,6 @@ public class RicercaProdottiView extends BaseView {
 
 		return null;
 	}
-		
-	
 
 	public List<AttributeDTO> completeGenere(String query) {
 		return getAutoUtils().completeGenere(query);
@@ -131,17 +128,13 @@ public class RicercaProdottiView extends BaseView {
 
 	public void findProducts(ActionEvent e) {
 		try {
-			if (merchantBean.getDtBegin()!=null)
-			{
-				if (merchantBean.getDtEnd()==null)
-				{
+			if (merchantBean.getDtBegin() != null) {
+				if (merchantBean.getDtEnd() == null) {
 					addError("Date finale errata", "inserire la data");
 				}
 			}
-			if (merchantBean.getDtEnd()!=null)
-			{
-				if (merchantBean.getDtBegin()==null)
-				{
+			if (merchantBean.getDtEnd() != null) {
+				if (merchantBean.getDtBegin() == null) {
 					addError("Date iniziale errata", "inserire la data");
 				}
 			}
@@ -151,6 +144,71 @@ public class RicercaProdottiView extends BaseView {
 		}
 	}
 
+	
+	
+			
+			
+			public void modificaProdotto(ActionEvent e) {
+
+		try {
+
+			if (merchantBean.getSelectedProducts() != null
+					&& merchantBean.getSelectedProducts().size() == 1) {
+
+				if (merchantBean.getSelectedProducts().get(0).getTyProduct() == 1) {
+					FacesContext
+							.getCurrentInstance()
+							.getExternalContext()
+							.redirect(
+									"insertProdotti2.xhtml?faces-redirect=true&;detail=true&;mod=true");
+
+				} else {
+					FacesContext
+							.getCurrentInstance()
+							.getExternalContext()
+							.redirect(
+									"insertProdotti.xhtml?faces-redirect=true&;detail=true&;mod=true");
+
+				}
+			}
+		} catch (IOException ex) {
+			// TODO Auto-generated catch block
+			addGenericError(ex, "errore redirect dettaglio prodotto");
+		}
+
+	}
+	
+	
+	public void vediProdotto(ActionEvent e) {
+
+		try {
+
+			if (merchantBean.getSelectedProducts() != null
+					&& merchantBean.getSelectedProducts().size() == 1) {
+
+				if (merchantBean.getSelectedProducts().get(0).getTyProduct() == 1) {
+					FacesContext
+							.getCurrentInstance()
+							.getExternalContext()
+							.redirect(
+									"insertProdotti2.xhtml?faces-redirect=true&detail=true&read=true");
+
+				} else {
+					FacesContext
+							.getCurrentInstance()
+							.getExternalContext()
+							.redirect(
+									"insertProdotti.xhtml?faces-redirect=true&detail=true&;read=true");
+
+				}
+			}
+		} catch (IOException ex) {
+			// TODO Auto-generated catch block
+			addGenericError(ex, "errore redirect dettaglio prodotto");
+		}
+
+	}
+
 	public void eliminaProdotto(ActionEvent e) {
 		int del = 0;
 		try {
@@ -158,13 +216,16 @@ public class RicercaProdottiView extends BaseView {
 			if (merchantBean.getSelectedProducts() != null) {
 				for (ProductFullNewDTO prd : merchantBean.getSelectedProducts()) {
 
-					List<String> imageToDelete = OthalaFactory.getProductServiceInstance().deleteProduct(
-							prd.getIdProduct());
+					List<String> imageToDelete = OthalaFactory
+							.getProductServiceInstance().deleteProduct(
+									prd.getIdProduct());
 					ResizeImageUtil.deleteImages(imageToDelete);
 					del++;
 				}
 				addInfo("Eliminazione Prodotto",
-						String.format("operazione eseguita correttamente, eliminati  %d prodotti", del));
+						String.format(
+								"operazione eseguita correttamente, eliminati  %d prodotti",
+								del));
 				getBeanApplication().resetDomain();
 				findProducts(null);
 				merchantBean.setSelectedProducts(null);
@@ -173,8 +234,11 @@ public class RicercaProdottiView extends BaseView {
 			}
 
 		} catch (Exception ex) {
-			addGenericError(ex,
-					String.format("errore durante la cancellazione del prodotto, eliminati  %d prodotti", del));
+			addGenericError(
+					ex,
+					String.format(
+							"errore durante la cancellazione del prodotto, eliminati  %d prodotti",
+							del));
 
 		}
 		if (del > 0) {
@@ -188,21 +252,26 @@ public class RicercaProdottiView extends BaseView {
 
 			List<Integer> idPrds = new ArrayList<>();
 			for (ProductFullNewDTO prd : merchantBean.getSelectedProducts()) {
-				if (prd.getIdProductState() == TypeStateProduct.INSERITO.getState()) {
+				if (prd.getIdProductState() == TypeStateProduct.INSERITO
+						.getState()) {
 					idPrds.add(prd.getIdProduct());
 				}
 
 			}
 			if (idPrds != null && idPrds.size() > 0) {
-				OthalaFactory.getProductServiceInstance().publishProduct(idPrds);
+				OthalaFactory.getProductServiceInstance()
+						.publishProduct(idPrds);
 
 				addInfo("Pubblicazione Prodotto",
-						String.format("operazione eseguita correttamente, pubblicati  %d prodotti", idPrds.size()));
+						String.format(
+								"operazione eseguita correttamente, pubblicati  %d prodotti",
+								idPrds.size()));
 				getBeanApplication().resetDomain();
 				findProducts(null);
-				
+
 			} else {
-				addError("Pubblicazione Prodotto", "nessun prodotto selezionato");
+				addError("Pubblicazione Prodotto",
+						"nessun prodotto selezionato");
 			}
 			merchantBean.setSelectedProducts(null);
 		} catch (Exception ex) {
@@ -211,16 +280,16 @@ public class RicercaProdottiView extends BaseView {
 	}
 
 	public void creaCampagna(ActionEvent e) {
-		String opCampagna = addToCampaign == false ? "Creazione Campagna" : "Modifica Campagna";
+		String opCampagna = addToCampaign == false ? "Creazione Campagna"
+				: "Modifica Campagna";
 		try {
 
-			if (campaignDTO.getDtInizio().compareTo(campaignDTO.getDtFine())>=0)
-			{
-				addError("Campagna", "data inizione deve essere inferiore a data fine");
+			if (campaignDTO.getDtInizio().compareTo(campaignDTO.getDtFine()) >= 0) {
+				addError("Campagna",
+						"data inizione deve essere inferiore a data fine");
 				return;
 			}
-			
-			
+
 			List<Integer> idPrds = new ArrayList<>();
 			for (ProductFullNewDTO prd : merchantBean.getSelectedProducts()) {
 
@@ -229,11 +298,15 @@ public class RicercaProdottiView extends BaseView {
 			}
 			if (idPrds != null && idPrds.size() > 0) {
 				if (!addToCampaign) {
-					OthalaFactory.getProductServiceInstance().insertCampaign(campaignDTO, idPrds);
+					OthalaFactory.getProductServiceInstance().insertCampaign(
+							campaignDTO, idPrds);
 				} else {
-					OthalaFactory.getProductServiceInstance().addProductToCampaign(idPrds, campaignDTO.getIdCampaign());
+					OthalaFactory.getProductServiceInstance()
+							.addProductToCampaign(idPrds,
+									campaignDTO.getIdCampaign());
 				}
-				addInfo(opCampagna, String.format("operazione eseguita correttamente"));
+				addInfo(opCampagna,
+						String.format("operazione eseguita correttamente"));
 
 			} else {
 				addError(opCampagna, "nessun prodotto selezionato");
@@ -246,28 +319,31 @@ public class RicercaProdottiView extends BaseView {
 	}
 
 	public void showCreaCampagna(ActionEvent e) {
-		campaignDTO=new CampaignDTO();
+		campaignDTO = new CampaignDTO();
 		campaignDTO.setPcSconto(1);
-		
+
 		addToCampaign = false;
-		RequestContext.getCurrentInstance().execute("PF('newCampaign').show();");
-		
+		RequestContext.getCurrentInstance()
+				.execute("PF('newCampaign').show();");
+
 	}
 
 	public void showAddCampagna(ActionEvent e) {
-		campaignDTO=null;
-		
+		campaignDTO = null;
+
 		addToCampaign = false;
 		campaigns = OthalaFactory.getProductServiceInstance().getListCampaign();
 		listCampaigns = new ArrayList<SelectItem>();
 		for (CampaignDTO c : campaigns) {
-			listCampaigns.add(new SelectItem(c.getIdCampaign(), c.getTxCampaign()));
+			listCampaigns.add(new SelectItem(c.getIdCampaign(), c
+					.getTxCampaign()));
 		}
 		if (listCampaigns.isEmpty()) {
 			addError("Aggiungi a Campagna", "Nessuna campagna presente");
 		} else {
 			addToCampaign = true;
-			RequestContext.getCurrentInstance().execute("PF('newCampaign').show();");
+			RequestContext.getCurrentInstance().execute(
+					"PF('newCampaign').show();");
 		}
 	}
 
